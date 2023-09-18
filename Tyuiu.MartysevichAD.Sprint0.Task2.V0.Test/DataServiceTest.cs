24588e4c556960bcfb116667f1367e8ac6c78168
Tyuiu.MartysevichAD <Tyuiu.MartysevichAD@outlook.com>
2023-09-18 21:44:42 +0500

    Task2

---
+++ Tyuiu.MartysevichAD.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.MartysevichAD.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.MartysevichAD.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckgetMessageValid()
+        {
+            var name = "Андрей";
+            var res = DataService.GetMessage(name);
+
+
+            Assert.AreEqual("Привет, Андрей", res);
+        }
+    }
+}
