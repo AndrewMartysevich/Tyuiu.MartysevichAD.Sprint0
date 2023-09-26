4f160d10f8d2221b90f1b701c3c55fea1604a06b
Tyuiu.MartysevichAD <Tyuiu.MartysevichAD@outlook.com>
2023-09-26 23:50:04 +0500

    Task5

---
+++ Tyuiu.MartysevichAD.Sprint0.Task5.Lib/DataService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.MartysevichAD.Sprint0.Task5.Lib
+{
+    public class DataService
+    {
+        public static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Substraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static double Division(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
